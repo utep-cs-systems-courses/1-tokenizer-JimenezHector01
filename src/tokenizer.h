44d7ad3a9ca14bc7f9d08da925cfbee659f9f938
JimenezHector01 <hjjimenez@miners.utep.edu>
2023-05-04 20:45:31 -0600

    update

--- src/tokenizer.h
+++ src/tokenizer.h
@@ -2,46 +2,61 @@
 #define _TOKENIZER_
 
 
-/* Return true (non-zero) if c is a whitespace characer
-   ('\t' or ' ').  
-   Zero terminators are not printable (therefore false) */
 int space_char(char c);
+   return (c == " ");
 
-/* Return true (non-zero) if c is a non-whitespace 
-   character (not tab or space).  
-   Zero terminators are not printable (therefore false) */ 
 int non_space_char(char c);
+   return (c != " ");
 
-/* Returns a pointer to the first character of the next 
-   space-separated word in zero-terminated str.  Return a zero pointer if 
-   str does not contain any words. */
 char *word_start(char *str); 
+   return (str == "\0")
 
-/* Returns a pointer terminator char following *word */
-char *word_terminator(char *word);
+char *word_terminator(char *word){
+     while(non_space_char(word)){
+       word++;
+     }
+     return word;
+}
 
-/* Counts the number of words in the string argument. */
-int count_words(char *str);
+int count_words(char *str){
+  int count = 0;
+  while(str != "\0"){
+    str = word_start(str);
+    str = word_terminator(str);
+    count++;
+  }
+  return count;
+}
 
-/* Returns a fresly allocated new zero-terminated string 
-   containing <len> chars from <inStr> */
-char *copy_str(char *inStr, short len);
+char *copy_str(char *inStr, short len){
+  char *copyStr;
+  int i;
+  for (i = 0; i < len; i++) {
+    copyStr[i] = inStr[i];
+  }
+  copyStr[i] = '\0';
+  return copyStr;
+}
 
-/* Returns a freshly allocated zero-terminated vector of freshly allocated 
-   space-separated tokens from zero-terminated str.
+char **tokenize(char* str){
+  int word_size = count_words(str);
+  char **words;
+  int i = 0;
+  for (i = 0; i < word_size; i++) {
+    char* start = word_start(str);
+    str = word_terminator(start);
+    words[i] = copy_str(start, str - start);
+  }
+  words[i] = NULL;
+  return words;
+}
 
-   For example, tokenize("hello world string") would result in:
-     tokens[0] = "hello"
-     tokens[1] = "world"
-     tokens[2] = "string" 
-     tokens[3] = 0
-*/
-char **tokenize(char* str);
+void print_tokens(char **tokens){
+  for (int i = 0; tokens[i] != 0; i++) {
+    printf("\nTokens[%d] : %s\n", i, tokens[i]);
+  }
+}
 
-/* Prints all tokens. */
-void print_tokens(char **tokens);
-
-/* Frees all tokens and the vector containing themx. */
 void free_tokens(char **tokens);
 
 #endif
